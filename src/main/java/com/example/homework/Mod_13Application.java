60c70bf1a46be0141632d464918262005b831598
Artem1548 <140644935+Artem1548@users.noreply.github.com>
2024-01-03 16:10:00 +0200

    Add files via upload

---
+++ src/main/java/com/example/homework/Mod_13Application.java
@@ -0,0 +1,13 @@
+package com.example.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mod_13Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mod_13Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homework/Mod_13ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.example.homework;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Mod_13ApplicationTest {
+
+    @Test
+    void main() {
+    }
+}
